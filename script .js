bb37c63f7d7e19113cc9232fc834a02efe514329
Yash Rajput <rajputyash8561@gmail.com>
2024-05-20 23:12:41 +0530

    store user data into server

---
+++ script .js
@@ -0,0 +1,10 @@
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        event.preventDefault();
+        alert('All fields are required.');
+    }
+});
